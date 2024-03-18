aa11654226cc9385788f29fc4e44fd7a3dea426b
OmKumarBhardwaj <om989.hitcse2020@gmail.com>
2024-03-18 19:10:29 +0530

    created Badge

--- src/main/java/org/coder/coderhack/constant/Badge.java
+++ src/main/java/org/coder/coderhack/constant/Badge.java
@@ -1,4 +1,7 @@
 package org.coder.coderhack.constant;
 
 public enum Badge {
+    CODE_NINJA, // 1 <= score <= 30
+    CODE_CHAMP, // 30 <= score <= 60
+    CODE_MASTER // 60 <= score <= 100
 }
--- src/main/java/org/coder/coderhack/entity/User.java
+++ src/main/java/org/coder/coderhack/entity/User.java
@@ -1,5 +1,10 @@
 package org.coder.coderhack.entity;
 
+import org.coder.coderhack.constant.Badge;
+
+import java.util.Set;
+
+
 public class User {
     private String userId;
     private String userName;
