8585abdaa9099aff2fafe80b158727d53fd6c274
OmKumarBhardwaj <om989.hitcse2020@gmail.com>
2024-03-19 03:04:47 +0530

    Created Post Method to register user

---
+++ src/main/java/org/coder/coderhack/constant/StatusCode.java
@@ -0,0 +1,15 @@
+package org.coder.coderhack.constant;
+
+public class StatusCode {
+    private StatusCode() {
+    }
+    private static final String STATUS_201 = "201";
+    private static final String MESSAGE_201 = "User Registered Successfully";
+    private static final String STATUS_200 = "200";
+    private static final String MESSAGE_200 = "Request Processed Successfully";
+    private static final String STATUS_500 = "500";
+    private static final String MESSAGE_500 = "Internal Server Error";
+    private static final String STATUS_417 = "417";
+    private static final String MESSAGE_417 = "User Not Found, Update cannot be performed";
+
+}
--- src/main/java/org/coder/coderhack/controller/UserController.java
+++ src/main/java/org/coder/coderhack/controller/UserController.java
@@ -2,8 +2,12 @@ package org.coder.coderhack.controller;
 
 
 import lombok.AllArgsConstructor;
+import org.coder.coderhack.dto.ResponseDto;
+import org.coder.coderhack.dto.UserRegistrationDto;
 import org.coder.coderhack.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private UserService userservice;
 
-    public ResponseEntity<>
+    @PostMapping
+    public ResponseEntity<ResponseDto> registerUser(@RequestBody UserRegistrationDto user) {
+        userservice.registerUser(user);
+    }
 }
