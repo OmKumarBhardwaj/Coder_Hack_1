e52d533b0e66c5cc603cbd585104d7f0245db716
OmKumarBhardwaj <om989.hitcse2020@gmail.com>
2024-03-21 15:57:40 +0530

    Created GlobalException Handler

--- src/main/java/org/coder/coderhack/controller/UserController.java
+++ src/main/java/org/coder/coderhack/controller/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private UserService userservice;
 
-    @PostMapping
+    @PostMapping("/register")
     public ResponseEntity<ResponseDto> registerUser(@RequestBody UserRegistrationDto user) {
         userservice.registerUser(user);
         return ResponseEntity
---
+++ src/main/java/org/coder/coderhack/exception/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package org.coder.coderhack.exception;
+
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+}
